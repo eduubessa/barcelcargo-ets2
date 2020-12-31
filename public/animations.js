1496fdf4d9af3724548956046ad97aa8cd92236a
Eduardo Bessa <eduubessa@gmail.com>
2020-12-31 02:25:13 +0000

    big upgrade and new organization on project

--- public/animations.js
+++
@@ -1,6 +0,0 @@
-function numberIncrease(element, start, end, duration)
-{
-    let timestampsStart = 0;
-
-
-}
--- src/components/pages/errors/NotFoundPage.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div>
-    <section class="error">
-      <h1>Error 404</h1>
-    </section>
-  </div>
-</template>
